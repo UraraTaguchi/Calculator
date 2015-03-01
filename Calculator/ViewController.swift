ea02cb774395ca489a5281c1ada832478fe95523
Urara Taguchi <urara.rarara.0312@gmail.com>
2015-03-01 17:10:34 +0900

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by 田口うらら on 2015/03/01.
+//  Copyright (c) 2015年 田口うらら. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
